6789a7cd084c51da5d2dc42dd65481f08b0648e4
Sean C Davis <scdavis41@gmail.com>
2025-04-03 10:40:27 -0400

    Add config for deploying to Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server'
+  output: 'server',
+  adapter: netlify()
 });
